28e595ab72eb01c9f4b4ff98218051f76c817b43
razan <razanmonther6@gmail.com>
2024-06-25 22:48:08 +0300

    Initial commit

---
+++ src/test/java/soft/progress/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.progress.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
